4381bf9ebe905a6c103173438890c5f5197f3cfb
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-07 09:48:52 +0200

    Open-Lab-01.1

--- Open-Lab-01.1/Program.cs
+++ Open-Lab-01.1/Program.cs
@@ -6,7 +6,20 @@ namespace Open_Lab_01._1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a < 10)
+            {
+                Console.WriteLine("This number is less then 10.");
+            }
+            else if (a == 10)
+            {
+                Console.WriteLine("This number is equal to 10.");
+
+            }
+            else
+            {
+                Console.WriteLine("This number is greater to 10.");
+            }
         }
     }
 }
