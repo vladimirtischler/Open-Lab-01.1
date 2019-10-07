9b53233b9e86aa12fbddbbbb6a4ef7cf0a9d1704
z18ista30 <z18ista30@Z00P1-11.SPSKNM>
2019-10-07 09:18:48 +0200

    Add project files.

---
+++ Open-Lab-01.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Open_Lab_01._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
